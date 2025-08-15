ef350daa9902d7736ab41c1ddec9fa1f8f71baad
mananpGit <mvpatel8605@gmail.com>
2025-08-15 00:15:51 -0500

    style chnages as we as rendering week and saved list

--- frontend/src/pages/MealPlanner.jsx
+++ frontend/src/pages/MealPlanner.jsx
@@ -4,6 +4,7 @@ import DayCard from "../components/DayCard";
 import "./styles/MealPlanner.css";
 
 function MealPlanner() {
+    //get empty/inital week
     const createEmptyWeek = () => {
         const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
         const emptyWeek = {};
@@ -16,13 +17,27 @@ function MealPlanner() {
     };
 
     const [mealPlan, setMealPlan] = useState(createEmptyWeek());
+    const [saved, setSaved] = useState([]);
+
+    useEffect(() => {
+        setSaved(getSavedRecipes());
+    }, []);
 
     return (
         <div className="meal-planner">
             <h1>Weekly Meal Planner</h1>
 
             <div className="saved-list">
-
+                {saved.length > 0 ? (
+                    saved.map((recipe) => (
+                        <div  key={recipe.id} className="sidebar-recipe-item" draggable={true}>
+                            <img src={recipe.image} alt={recipe.title} />
+                            <span>{recipe.title}</span>
+                        </div>
+                    ))
+                ) : (
+                    <h2>No Recipes Saved</h2> 
+                )}
             </div>
 
             <div className="weekly-grid">
